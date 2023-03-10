24539d5f84e37b6d6758be80b749d1ecd85e4f98
Krste-k <krstevskikrste89@yahoo.com>
2023-03-10 08:38:44 +0100

    Homework-class-1-C#

---
+++ Homework-class-1/Homework-class-1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, SEDC!");
+
+Console.WriteLine(" --------------------------------------------------------|");
+Console.WriteLine(" |       *         ___________  ___/___                  |");
+Console.WriteLine(" |       **        |    |    |  |  C#  |      ________   |");
+Console.WriteLine(" |      ****       |____|____|  |______|     |        |  |");
+Console.WriteLine(" |     ******      |    |    |               |        |  |");
+Console.WriteLine(" |    ********     |____|____|               |_       |  |");
+Console.WriteLine(" |   **********                              |        |  |");
+Console.WriteLine(" |      * *                                  |        |  |");
+Console.WriteLine(" |______* *__________________________________|________|__|");
+Console.WriteLine(" |       ______________________________________          |");
+Console.WriteLine(" |      |                                      |         |");
+Console.WriteLine(" |      |                                      |         |");
+Console.WriteLine(" |      |                                      |         |");
+Console.WriteLine(" |      |                                      |         |");
+Console.WriteLine(" |      |                                      |         |");
+Console.WriteLine(" |      |______________________________________|         |");
---
+++ Homework-class-1/HomeworkClas1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+string homework = "HOMEWORK 1.1";
+Console.WriteLine(homework);
+
+
+string strNum1 = "0000000111111111111111111110000000000000000011100001110000000000000000000";
+string strNum4 = "0000000111111000000000000000000000000000000000000000000000000000000000000";
+string strNum5 = "0000000000000000000000000000000000000000000000000000000000000000000000000";
+string strNum2 = "0000000111111000000000000000000000000000000011100001110000000000000000000";
+string strNum = "0000000111111111111111111110000000000000000000000000000000000000000000000";
+string strNum3 = "0000000111111000000000000000000000000000111111111111111111000000000000000";
+
+
+
+
+Console.WriteLine(strNum5);
+Console.WriteLine(strNum);
+Console.WriteLine(strNum1);
+Console.WriteLine(strNum2);
+Console.WriteLine(strNum3);
+Console.WriteLine(strNum2);
+Console.WriteLine(strNum3);
+Console.WriteLine(strNum2);
+Console.WriteLine(strNum2);
+Console.WriteLine(strNum4);
+Console.WriteLine(strNum4);
+Console.WriteLine(strNum4);
+Console.WriteLine(strNum4);
+Console.WriteLine(strNum);
+Console.WriteLine(strNum);
+Console.WriteLine(strNum5);
+
+
