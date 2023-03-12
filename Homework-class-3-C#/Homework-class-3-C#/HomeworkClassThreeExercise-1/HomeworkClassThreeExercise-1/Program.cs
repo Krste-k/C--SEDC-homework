ff5c09f5da41baba9fbfe6b4cabc95d5e3f17292
Krste-k <krstevskikrste89@yahoo.com>
2023-03-12 23:39:19 +0100

    Homework-class-3-C#

---
+++ Homework-class-3-C#/Homework-class-3-C#/HomeworkClassThreeExercise-1/HomeworkClassThreeExercise-1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Create a console application "SwapNumbers".
+* Input 2 numbers from the console and then swap the values of the variables 
+* so that the first variable has the second value and the second variable the first value.
+* Please find examples below:
+///
+* Test Data:
+* Input the First Number: 5
+* Input the Second Number: 8
+* Expected Output:
+* After Swapping:
+* First Number: 8
+* Second Number: 5*/
+
+
+Console.WriteLine("====================================================");
+
+Console.WriteLine("===== 'Enter the first number' =====");
+String inputOne = Console.ReadLine(); //nema potreba da se parsira za zadacava!
+Console.WriteLine(" The first value from the input is " + inputOne);
+
+Console.WriteLine("====================================================");
+Console.WriteLine("===== 'Enter the second number' =====");
+String inputTwo = Console.ReadLine();
+Console.WriteLine(" the second value from the input is " + inputTwo);
+
+string empty ;
+
+Console.WriteLine("====================================================");
+Console.WriteLine("Press enter to swap the values ");
+Console.ReadLine();
+
+// empty ja dobiva vrednosta na inputOne
+empty = inputOne;
+// inputOne ja dobivra vrednosta na inputTwo
+inputOne = inputTwo;
+// inputTwo ja zema vrednosta na inputOne koja sto veke ja prefrlivme pogore vo empty
+inputTwo = empty;
+
+Console.WriteLine("From the first input new value is " + inputOne);
+Console.WriteLine("From the second input new value is " + inputTwo);
+
+
---
+++ Homework-class-3-C#/Homework-class-3-C#/HomeworkClassThreeExercise-1/HomeworkClassThreeExercise-2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Make a console application called SumOfEven. Inside it create an array of 6 integers.
+ * Get numbers from the input, find and print the sum of the even numbers from the array:
+*/
+
+int[] ArrOfnum = new int[6];
+Console.WriteLine("Enter 6 numbers ");
+for (int i = 0; i < 6; i++)
+{
+    ArrOfnum[i] = int.Parse(Console.ReadLine());
+}
+
+int sumOfEven = 0;
+foreach (int num in ArrOfnum)
+{
+    if (num % 2 == 0)
+    {
+        sumOfEven += num; 
+
+        
+        if (sumOfEven <= 20)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+        else if (sumOfEven > 20)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+        }
+        else if (sumOfEven >= 40)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+        }
+        else if (sumOfEven >= 60)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+        }
+        else if (sumOfEven >= 80)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+        }
+        Console.ForegroundColor = ConsoleColor.Black;
+    }
+    
+       
+}
+
+
+Console.WriteLine("The sum of even numbers is: " + sumOfEven);
